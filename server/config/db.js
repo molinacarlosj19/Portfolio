76144d2ed461e67fd392a28ae16007193fcc10c8
Carlos Molina <molinacarlosj@gmail.com>
2022-06-27 17:25:17 -0500

    Modify db conexion

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://molinacarlosj:ifql77L1bcqSzUWt@cluster0.2nic0.mongodb.net/books229?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
+    //"URI": "mongodb://localhost:27017/portfolio"
+    //"URI": "mongodb+srv://molinacarlosj:Dejesus282006,.@cluster0.2nic0.mongodb.net/portfolio?retryWrites=true&w=majority"
+
 }
