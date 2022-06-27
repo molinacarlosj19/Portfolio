958b0fda54fd136ec2300405b21bccfc3840a1d1
Carlos Molina <molinacarlosj@gmail.com>
2022-06-27 15:56:44 -0500

    Add styles, content and refactor

--- server.js
+++ server.js
@@ -5,14 +5,14 @@
  */
 
 var app = require('./server/config/app');
-var debug = require('debug')('week-4:server');
+var debug = require('debug')('Portfolio:server');
 var http = require('http');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3500');
+var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
